0cf0bb5cd334a3ccbf50d996f854016183ae097b
Michał Budziak <michal.mariusz.b@gmail.com>
2017-04-23 19:13:25 +0200

    SECTORD17-23 #time 1h Implement servers endpoints

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ let process = require('process'),
     raven = require('raven'),
     path = require('path'),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
-
-let index = require('./routes/index');
-
-let app = express();
+    bodyParser = require('body-parser'),
+    app = express();
 
 env(__dirname + '/.env');
 
@@ -16,11 +13,17 @@ if (process.env.SENTRY_DSN) {
     raven.config(process.env.SENTRY_DSN).install();
 }
 
+app.debug = function (message) {
+    if (process.env.APP_DEBUG) {
+        console.log(message);
+    }
+};
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', index);
+require('./lib')(app);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -37,7 +40,7 @@ app.use(function (err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.send(null)
 });
 
 module.exports = app;
