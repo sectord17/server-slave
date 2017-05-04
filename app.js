61e040ad4d7832a12206ba9b7b57f7c8f73894bf
Michał Budziak <michal.mariusz.b@gmail.com>
2017-05-05 01:06:38 +0200

    SECTORD17-25 #time 10m Handle 500 error responses

--- app.js
+++ app.js
@@ -13,34 +13,26 @@ if (process.env.SENTRY_DSN) {
     raven.config(process.env.SENTRY_DSN).install();
 }
 
-app.debug = function (message) {
-    if (process.env.DEBUG) {
-        console.log(message);
-    }
-};
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 require('./lib')(app);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    let err = new Error('Not Found');
+app.use((req, res, next) => {
+    let err = new Error();
     err.status = 404;
     next(err);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+    let status = err.status || 500;
 
-    // render the error page
-    res.status(err.status || 500);
-    res.send(null)
+    if (err.message === "model-not-found") {
+        status = 404;
+    }
+
+    res.sendStatus(status);
 });
 
 module.exports = app;
