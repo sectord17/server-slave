be58835cfd2b00ed89ead067a5484c2a577173a1
Michał Budziak <michal.mariusz.b@gmail.com>
2017-10-22 22:42:01 +0200

    Refactor

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-let process = require('process'),
-    express = require('express'),
-    env = require('node-env-file'),
-    raven = require('raven'),
-    path = require('path'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
-    app = express();
-
-const BasicError = require('./lib/errors/basic-error');
-const ModelNotFoundError = require('./lib/errors/model-not-found-error');
-const EndpointNotFoundError = require('./lib/errors/endpoint-not-found-error');
+const env = require('node-env-file');
+const raven = require('raven');
+const winston = require('winston');
 
 env(__dirname + '/.env');
 
@@ -17,34 +8,6 @@ if (process.env.SENTRY_DSN) {
     raven.config(process.env.SENTRY_DSN).install();
 }
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+winston.level = 'debug';
 
-require('./lib')(app);
-
-app.use((req, res, next) => {
-    next(new EndpointNotFoundError());
-});
-
-app.use((error, request, response, next) => {
-    if (error instanceof EndpointNotFoundError) {
-        return response.sendStatus(404);
-    }
-
-    if (error instanceof BasicError) {
-        return response.status(403).send(error);
-    }
-
-    if (error instanceof ModelNotFoundError) {
-        return response.sendStatus(404);
-    }
-
-    if (process.env.SENTRY_DSN) {
-        raven.captureException(error);
-    }
-
-    response.sendStatus(500);
-});
-
-module.exports = app;
+require('./lib');
